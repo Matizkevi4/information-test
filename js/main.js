72a58078fb72efc70a44c6ff860cc0a61a944270
BlackIce18 <38867396+BlackIce18@users.noreply.github.com>
2018-04-30 23:06:40 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -23,18 +23,27 @@ $(document).ready(function() {
             $(this).addClass("navigator_item-active");
         }
     });*/
+    function swipe(nav_item){
+        $('.wrap').animate({
+            right:nav_item*100+'vw'
+        }, 1000);
+        var navig = '.navigator_item_slide_'+(nav_item+1);
+        $('.navigator_item-active').removeClass('navigator_item-active');
+        $(navig).addClass('navigator_item-active');
+    }
+    setInterval(function(){
+        if (slideNow == 4) { slideNow = -1;}
+        slideNow++;
+        swipe(slideNow);
+    },10000);
     function slider(elem) {
-    $(elem).on('click', function(){
-        var nav_item = $(this).index();
-        if ( nav_item != slideNow) {
-            $('.wrap').animate({
-                right:nav_item*100+'vw'
-            }, 1000);
-            slideNow = nav_item;
-            $('.navigator_item-active').removeClass('navigator_item-active');
-            $(this).addClass("navigator_item-active");
-        }
-    });
-}
+        $(elem).on('click', function(){
+            var nav_item = $(this).index();
+            if ( nav_item != slideNow) {
+                swipe(nav_item);
+                slideNow = nav_item;
+            }
+        });
+    }
     slider('.gallery_navigator_item');
 });
